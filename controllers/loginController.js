866da53443afda711dfefe2f39086c4776273fd0
Ryuzen <abangrianb@gmail.com>
2025-02-18 08:11:53 +0700

    P

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -11,22 +11,18 @@ exports.handleLogin = (req, res) => {
 
   // Query untuk mencari user berdasarkan ID
   const query = "SELECT * FROM ms_user WHERE id_user = ?";
-
   db.query(query, [id], async (err, results) => {
     if (err) {
       console.error("Database error:", err);
       return res.status(500).json({ error: "Internal server error." });
     }
-
     if (results.length === 0) {
       return res.status(401).json({ error: "Invalid credentials." });
     }
-
     const user = results[0];
 
     // Membandingkan password yang diberikan dengan password yang tersimpan di database
     const passwordMatch = await bcrypt.compare(password, user.password);
-
     if (!passwordMatch) {
       return res.status(401).json({ error: "Invalid credentials." });
     }
@@ -34,9 +30,15 @@ exports.handleLogin = (req, res) => {
     // Menyimpan data user di session (cookie)
     req.session.user = { id: user.id_user, name: user.FullName, isAdmin: user.isAdmin };
 
-    // Mengirim response sukses beserta data user
-    res.json({ message: "Login successful.", user: req.session.user });
-    console.log(req.session);  // Log the session to see if it's being set
+    // Kirim response sukses beserta data user untuk disimpan di localStorage
+    res.json({
+      message: "Login successful.",
+      user: {
+        id: user.id_user,
+        name: user.FullName,
+        isAdmin: user.isAdmin,
+      }
+    });
   });
 };
 
@@ -44,7 +46,7 @@ exports.handleLogout = (req, res) => {
   // Menghapus session untuk logout
   req.session = null;
 
-  // Mengirimkan response logout sukses
+  // Kirim response logout sukses
   res.json({ message: "Logout successful." });
 };
 
